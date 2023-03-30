f5f925389f68e4e19f80b50b2729e44f95799725
AndriiKot <a_kocyuba@ukr.net>
2023-03-30 10:26:25 +0200

    select and alias name

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -2,3 +2,41 @@
     *
 from 
 	actor;
+-----------------------------------------
+select 
+      actor_id
+from 
+	actor;
+
+----------------------------------------
+
+select
+      actor_id,
+      first_name,
+      last_name,
+      last_update
+from 
+      actor;
+
+------------------------------------------
+
+select
+      actor_id,
+      first_name,
+      last_name,
+      last_update,
+      *
+from
+      actor;
+-------------------------------------------
+
+select
+      actor_id actorIdentity,
+      first_name as firstName,
+      last_name "Last Name",
+      last_update as "Last Update"
+from 
+      actor;
+
+------------------------------------------
+
